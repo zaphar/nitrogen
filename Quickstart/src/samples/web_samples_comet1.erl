d65a1ae3285d8361727ccddd5a43b66740c6be7d
Tom McNulty <tom.mcnulty@cetiforge.com>
2009-04-15 01:16:35 +0800

    Comet Example 1 converted
    Signed-off-by: Jeremy Wall <jeremy@marzhillstudios.com>

--- Quickstart/src/samples/web_samples_comet1.erl
+++ Quickstart/src/samples/web_samples_comet1.erl
@@ -18,25 +18,27 @@ body() ->
   ],
 
 	% Start the counter as a background process.
-	wf:comet(fun() -> background_update(myCounter, 1) end),
-	
+	Agent = wf:comet2(fun background_update/1),
+	spawn(fun() -> link(Agent), timer(Agent, myCounter, 1) end),
 	wf:render(Body).
 
 event(_) -> ok.
 
-background_update(ControlID, Count) ->
+
+timer(Agent, ControlID, Count) ->
 	% Sleep for a second, then update
 	timer:sleep(1000),
-	
-	% Update the control.
-	wf:update(ControlID, wf:to_list(Count)),
-	
-	% wf:comet_flush() is only needed because we are looping. Otherwise,
-	% we could just let the function complete.
-	wf:comet_flush(),
+
+	Agent ! {ControlID, Count},
 	
 	% Loop. This process will automatically be killed
 	% once the page stops requesting the output that
 	% it generates.
-	background_update(ControlID, Count + 1).
+	timer(Agent, ControlID, Count + 1).
+	
+
+
+background_update({ControlID, Count}) ->
+	% Update the control.
+	wf:update(ControlID, wf:to_list(Count)).
 	
